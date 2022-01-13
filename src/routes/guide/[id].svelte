19e54e57eeedfe0400d7d5629a2475f80d6d260d
Sithum Basnayaka <sithum.dashantha@gmail.com>
2022-01-13 07:49:36 +0530

    dynamic routing

---
+++ src/routes/guide/[id].svelte
@@ -0,0 +1,44 @@
+<script context="module">
+  export async function load({ fetch, page }) {
+    const id = page.params.id;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const guide = await res.json();
+
+    if (res.ok) {
+      return {
+        props: {
+          guide,
+        },
+      };
+    }
+
+    return {
+      status: res.status,
+      error: new Error("Could not load the data from the API"),
+    };
+  }
+</script>
+
+<script>
+  export let guide;
+</script>
+
+<div class="guide">
+  <h1>{guide.title}</h1>
+  <p>{guide.body}</p>
+</div>
+
+<style>
+  .guide {
+    max-width: 960px;
+    margin: 4rem auto;
+    font-family: "Montserrat", sans-serif;
+    font-size: 3vw;
+    color: blue;
+    border: 1px dotted #0000ff;
+    padding: 3rem;
+  }
+  p {
+    margin-top: 2rem;
+  }
+</style>
